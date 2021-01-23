9ca6bf04744b75b35b90d9398b28f812c721b98e
JerryKaura <jerimkaura001@gmail.com>
2021-01-23 16:51:11 +0300

    minor fix on the app folder

--- portfolio/remake/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(window).scroll(function() {
-        if(this.scrollY > 28){
-            $('.navbar').addClass("sticky");
-        }else{
-             $(".navbar").removeClass("sticky");   
-        }
-    })
-})
