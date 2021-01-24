7437b9b740050796bc6256eeb4f89b2570c943ae
JerryKaura <jerimkaura001@gmail.com>
2021-01-24 15:34:45 +0300

    Services Section

--- portfolio/remake/script.js
+++ portfolio/remake/script.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
-    $(window).scroll(function() {
-        if(this.scrollY > 28){
-            $('.navbar').addClass("sticky");
-        }else{
-             $(".navbar").removeClass("sticky");   
+    $(window).scroll(function () {
+        if (this.scrollY > 28) {
+            $(".navbar").addClass("sticky");
+        }else {
+            $(".navbar").removeClass("sticky");
         }
-    })
+    });
+    // toggle navbar menu
+    $(".menu-btn").click(function () {
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
+    });
 })
